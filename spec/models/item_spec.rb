39d253f15d86b0baadbb0718d5af3932629afb83
tmgg1212 <75664927+tmgg1212@users.noreply.github.com>
2021-01-19 15:53:04 +0900

    商品出品機能実装7

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -81,12 +81,12 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Price can't be blank")
     end
     it '価格の範囲が、¥300~¥9,999,999の間であること' do
-      @item.price = '299'
+      @item.price = 299
       @item.valid?
       expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
     end
     it '価格の範囲が、¥300~¥9,999,999の間であること' do
-      @item.price = '10000000'
+      @item.price = 10000000
       @item.valid?
       expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
     end
