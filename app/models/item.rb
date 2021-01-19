c105d721f65dfa3ee2c45da5bb91c1e99d943003
tmgg1212 <75664927+tmgg1212@users.noreply.github.com>
2021-01-19 12:08:34 +0900

    商品出品機能実装3

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,28 +3,28 @@ class Item < ApplicationRecord
     validates :name
     validates :price, numericality: { only_integer: true },
                       numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-    validates :category_id, numericality: { other_than: 0 }
-    validates :condition_id, numericality: { other_than: 0 }
-    validates :charges_id, numericality: { other_than: 0 }
-    validates :area_id, numericality: { other_than: 0 }
-    validates :schedule_id, numericality: { other_than: 0 }
+    validates :category_id
+    validates :condition_id
+    validates :charges_id
+    validates :area_id
+    validates :schedule_id
     validates :explanation
     validates :image
   end
 
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :condition_id
+    validates :charges_id
+    validates :area_id
+    validates :schedule_id
+  end
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :condition
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :charges
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :area
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :schedule
 
   has_one_attached :image
