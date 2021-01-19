adb270513d8c96ef3eb1ad376c73e69221edfbca
tmgg1212 <75664927+tmgg1212@users.noreply.github.com>
2021-01-19 20:42:04 +0900

    商品削除機能実装4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,8 +23,9 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    if item.destroy
-    redirect_to root_path
+    if @item.user == current_user
+      item.destroy
+      redirect_to root_path
     end
   end
 
