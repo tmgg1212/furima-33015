6ae9739b677a997c1c6c88d89425740a795ba7a3
tmgg1212 <75664927+tmgg1212@users.noreply.github.com>
2021-01-19 20:17:59 +0900

    商品削除機能実装1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,13 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      redirect_to root_path
+  end
+end
+
   private
 
   def item_params
